e0347c71149371b05f4312415b7362149317f006
Andres Menendez <andmenendez@gmail.com>
2022-06-30 00:37:56 -0400

    fixed issue

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<?php header( 'Location: /index.html' ) ;  ?>
-
-<?php
-
-$request = $_SERVER['REQUEST_URI'];
-
-switch ($request) {
-    case '/old' :
-        require __DIR__ . '/index.html';
-        break;
-    default:
-        http_response_code(404);
-        require __DIR__ . '/portfolio.html';
-        break;
-}
-
-?>
+<?php header( 'Location: /home.html' ) ;  ?>
